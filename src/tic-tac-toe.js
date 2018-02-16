09dfaa9ec2132025c766fdcba6d3ffa9a5b7f839
moteo <moteoo@gmail.com>
2018-02-16 03:05:43 +0300

    COMPLETE!

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -5,8 +5,6 @@ class TicTacToe {
      this.winner = null;
      this.count = 0;
      this.finished = false;
-
-
    }
 
    getCurrentPlayerSymbol() {
@@ -16,87 +14,85 @@ class TicTacToe {
    }
 
    nextTurn(rowIndex, columnIndex) {
+  var i = 0;
+  var j = 0;
      if(this.t[rowIndex][columnIndex] == undefined && this.count <=9) {
      this.t[rowIndex][columnIndex] = this.symbol;
 
    if (this.symbol == 'x') {this.symbol = 'o'}
    else {this.symbol = 'x'}
-   this.count++
+  this.count++
+  
 
    }
 ///////////////////////////////////
-if (this.count == 9) {
-for(var i=0; i<4; i++) {
-       if(this.t[i][0]==this.t[i][1] && this.t[i][1]==this.t[i][2]) {
-         console.log("winnn1");
-         this.winner = this.t[i][0];
-         this.finished = true;
-       console.log(this.winner, "AAAAAAAAAAA");
-       //return this.winner
-       }
-       else if (this.t[0][i]==this.t[1][i] && this.t[1][i]==this.t[2][i]){
-         this.winner = this.t[0][i];
-         console.log("winnn2");
-      this.finished = true;
-      console.log(this.winner, "AAAAAAAAAAA");
-      //return this.winner
-       }
-       else if (this.t[0][0]==this.t[1][1] && this.t[1][1]==this.t[2][2]){
-         this.winner = this.t[0][0];
-         console.log("winnn3");
-      this.finished = true;
-      console.log(this.winner, "AAAAAAAAAAA");
-      //return this.winner
+//while (i<=2 ) {
+//console.log(this.t[0][0],this.t[0][1],this.t[0][2])
+//console.log(this.t[1][0],this.t[1][1],this.t[1][2])
+//console.log(this.t[2][0],this.t[2][1],this.t[2][2])
+for(var i=0; i<=2; i++) {
+       //console.log(i)
+       if(this.t[i][0]==this.t[i][1] && this.t[i][1]==this.t[i][2] && this.t[i][1] !=undefined) {
+        //console.log(this.t[i][0], "test1")
+
+          this.winner = this.t[i][0];
 
        }
-       else if (this.t[0][2]==this.t[1][1] && this.t[1][1]==this.t[2][0]){
-         this.winner = this.t[0][2];
-         console.log("winnn4");
-       this.finished = true;
-       console.log(this.winner, "AAAAAAAAAAA");
-       //return this.winner
+       else if (this.t[0][i]==this.t[1][i] && this.t[1][i]==this.t[2][i] && this.t[1][i] !=undefined){
+      //  console.log(this.t[i][0], "test2")
+
+            this.winner = this.t[0][i];
+
        }
-console.log(this.count, this.t, this.winner)
-return this.count, this.t, this.winner, this.symbol
+       else if (this.t[0][0]==this.t[1][1] && this.t[1][1]==this.t[2][2] && this.t[i][1] !=undefined){
+    //  console.log(this.t[1][1], "test3")
+ 
+            this.winner = this.t[1][1];
+
+       }
+      else if (this.t[2][0]==this.t[1][1] && this.t[1][1]==this.t[0][2] && this.t[i][1] !=undefined){
+    //  console.log(this.t[1][1], "test4")
+
+
+            this.winner = this.t[1][1];
+
+       }
+
+}
+//console.log(this.count, this.winner, "loop")
+return this.winner, this.t, this.symbol
      }
+//////////////////////////////////////////
 
-}
-//////////////////////////////////
-
-
-
-}
 
    isFinished() {
-     if(this.winner == null && this.count <9) {
-       return null
-     }
-     return this.winner
+//console.log(this.count, this.t,this.winner,"finished")
+     if(this.winner != null || this.count == 9) {
+//  console.log("TRUE")
+  return true}
+//  console.log("FALSE")
+  return false
+
 }
    
 
    getWinner() {
-if(this.winner != null) {
+  if(this.winner == undefined) {
+    return null
+  }
   return this.winner
 }
-return null
-   }
-
-   noMoreTurns() {
+   noMoreTurns() { //DONE
      if(this.count==9) {return true}
      return false
 
    }
 
-   isDraw() {
+   isDraw() { //DONE
      if( this.count == 9 && this.winner == null) {
-      //console.log('TRUE')
-      //console.log(this.winner)
        return true
 
      }
-      //console.log('FALSE')
-      //console.log(this.winner)
      return false
 
   }
